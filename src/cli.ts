9619fd96a2ad28d79db40f464601d6a48a69b916
Benny Shtemer <bs779779@gmail.com>
2025-05-29 11:48:49 +0300

    fix: update commander import to use @commander-js/extra-typings

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { Command } from '@commander-js/extra-typings';
 import { FhirPackageInstaller } from '.';
 
 // Set up the command line interface and global options
